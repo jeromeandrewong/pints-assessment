358bdc4df7632eecc2f847b8ba342a6517172454
Jerome Wong <jeromewongjh@gmail.com>
2023-03-16 22:28:37 +0800

    feat: implement api call on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,31 @@
 import express, { Request, Response } from "express";
 import axios from "axios";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+dotenv.config();
 
 const PORT = 5001,
   app = express();
 
-app.get("/api/v1", (req: Request, res: Response) => {
-  res.send("hello Backend");
+app.use(cors());
+app.use(bodyParser.json());
+const symbolArr = ["AAPL", "NFLX", "GOOG", "AMZN", "TSLA"];
+app.get("/api/v1", async (req: Request, res: Response) => {
+  const apiKey = process.env.IEX_API_TOKEN;
+
+  try {
+    const iexResponse = await axios.get(
+      `https://apis.iex.cloud/v1/data/CORE/QUOTE/aapl,nflx,goog,amzn,tsla?token=${apiKey}`
+    );
+    const logoUrlArr = symbolArr.map(
+      (symbol) => `https://storage.googleapis.com/iex/api/logos/${symbol}.png`
+    );
+
+    res.json([...iexResponse.data, logoUrlArr]);
+  } catch (error: unknown) {
+    if (error instanceof Error) res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => console.log(`start listening on port : ${PORT}`));
